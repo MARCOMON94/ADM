810e3ad3f372e20668fd340f87d48db2e07b768e
pepote94 <jcmm.1994@gmail.com>
2024-04-21 19:21:43 +0000

    videotutorial1

--- ADM/Assets/BATALLA/Enviroments/Square.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Square : MonoBehaviour
-{
-    public int costeDeMovimiento = 1; // Coste de movimiento para atravesar esta casilla.
-}
