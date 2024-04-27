2923308a48a41e05da2ac3cb2361ec9130d11fd2
pepote94 <jcmm.1994@gmail.com>
2024-04-27 09:15:26 +0000

    video 3.8

--- ADM/Assets/BATALLA/Maps/MenuScript.cs
+++ ADM/Assets/BATALLA/Maps/MenuScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEditor;
 public class MenuScript 
 {
+    /* crea en la barra superior un tools, que hace que todos los elementos con la etiqueta que 
+    le pongas ejecuten la acción que mandes, en este caso la de añadirle el scrip Tile*/
+    
     [MenuItem("Tools/Assign Tile Script")]
     public static void AssignTileScript()
     {
@@ -16,3 +19,4 @@ public class MenuScript
         }
     }
 }
+
